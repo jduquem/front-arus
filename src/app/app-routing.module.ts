6cf11887a80995d2f1c7eefa770b28a23d30f839
jduquem <alejandro.duque1104@gmail.com>
2022-10-11 01:02:20 -0500

    Inicio proyecto prueba arus

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PropietariosComponent } from './components/propietarios/propietarios.component';
+import { SolicitudesComponent } from './components/solicitudes/solicitudes.component';
+import { VehiculosComponent } from './components/vehiculos/vehiculos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'owners', component: PropietariosComponent},
+  { path: 'vehicles', component: VehiculosComponent},
+  { path: 'applications', component: SolicitudesComponent},
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VehiculosComponent } from './components/vehiculos/vehiculos.component';
+import { PropietariosComponent } from './components/propietarios/propietarios.component';
+import { SolicitudesComponent } from './components/solicitudes/solicitudes.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VehiculosComponent,
+    PropietariosComponent,
+    SolicitudesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
